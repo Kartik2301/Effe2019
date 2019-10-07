a73af54223abce66a62b550e7646ad74e2546bd5
Kartik Nema <knem7554@gmail.com>
2019-10-07 15:02:42 +0530

    Proshows

--- app/src/main/java/com/example/android/effe2019/ProshowsFragment.kt
+++ app/src/main/java/com/example/android/effe2019/ProshowsFragment.kt
@@ -5,70 +5,104 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kannan.glazy.GlazyCard
+import com.kannan.glazy.Utils
+import com.kannan.glazy.pager.GlazyFragmentPagerAdapter
+import com.kannan.glazy.transformers.GlazyPagerTransformer
+import com.kannan.glazy.views.GlazyImageView.ImageCutType
 import kotlinx.android.synthetic.main.fragment_proshows.*
+import androidx.fragment.app.FragmentPagerAdapter
 
 class ProshowsFragment : Fragment() {
-    val movies = arrayListOf<ProShow>()
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_proshows, container, false)
-        //Inflate the layout for this fragment
-        val movies = arrayListOf<ProShow>()
-
-
-//        for (i in resources.getStringArray(R.array.titles).indices) {
-//            movies.add(
-//                ProShow(
-//                  posters.getResourceId(i, -1),
-//                    resources.getStringArray(R.array.titles)[i],
-//                    resources.getStringArray(R.array.overviews)[i]
-//                R.drawable.icon,"hello","bye"
-//                )
-//            )
-//        }
-//        check_scaling.setOnCheckedChangeListener { _, isChecked ->
-//            if (isChecked) rootView.viewPager.smallScaleFactor =
-//                0.9f else rootView.viewPager.smallScaleFactor = 1f
-//        }
-//
-//        check_alpha_changing.setOnCheckedChangeListener { _, isChecked ->
-//            rootView.viewPager.smallAlphaFactor = if (isChecked) 0.5f else 1f
-//        }
-//
-//        check_enable_auto_sliding.setOnCheckedChangeListener { _, isChecked ->
-//            if (isChecked) rootView.viewPager.autoSlideTime =
-//                3 else rootView.viewPager.autoSlideTime = CardSliderViewPager.STOP_AUTO_SLIDING
-//        }
-//        check_infinite_indicator.setOnCheckedChangeListener { _, isChecked ->
-//            indicator.indicatorsToShow =
-//                if (isChecked) 5 else CardSliderIndicator.UNLIMITED_INDICATORS
-//        }
-        return rootView
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_proshows, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val posters = resources.obtainTypedArray(R.array.posters)
-
-
-        for (i in resources.getStringArray(R.array.titles).indices) {
-            movies.add(
-                ProShow(
-                   posters.getResourceId(i, -1),
-                   resources.getStringArray(R.array.titles)[i],
-                   resources.getStringArray(R.array.overviews)[i],
-                    resources.getStringArray(R.array.date)[i]
-                    )
-            )
-        }
-        posters.recycle()
-        viewPager.adapter = MovieAdapter(movies)
-
+        setupViewPager(view)
     }
-}
 
+    private fun setupViewPager(view: View) {
+        val resources = context!!.resources
+
+        val imgSunidhi = resources.getIdentifier("celebnight", "drawable", activity!!.packageName)
+        val imgHumour = resources.getIdentifier("humournight", "drawable", activity!!.packageName)
+        val imgEDM = resources.getIdentifier("edmnight", "drawable", activity!!.packageName)
+        val imgRock = resources.getIdentifier("localtrain", "drawable", activity!!.packageName)
+
+        val desc_celeb = "They say that when you become a celebrity ,you own the world and the world owns you. " +
+                "So get ready to own the world as the bar has been raised up higher. After superstars like Neha " +
+                "Kakkar,Benny Dayal and Farhan Akhtar, Effervescence’18 proudly presents to you Sunidhi Chauhan performing this October !"
+
+        val desc_edm = "EDM isn’t just about drops and rave, it is the form of music that has something to offer" +
+                " to everyone. Get ready to match you heartbeats with the beats and drops and dance your heart out " +
+                "with famous ZEPHYRTONE all set to raise the electric mood in the atmosphere!"
+
+        val desc_kavyom = "Gear up to enjoy the Infinite Sky of Poetry at Kavyom, being held for the very first " +
+                "time in IIIT Allahabad. Relish an indelible and a stupendous evening with the one and only, Kumar " +
+                "Vishwas. It is doubtlessly going to be the most congenial event for all poetry lovers out there."
+
+        val desc_humour = "The best thing about comedians apart from their wit and talent, is their confidence to bring " +
+                "out the mirth in small things! Known as one of the Rising Stars of Comedy in India, is one such comedian," +
+                " Aakash Gupta! We're bringing in the pros, to help you forget your woes! " +
+                "So, prepare to burst your lungs out and laugh to the rant of the leading stand up comedian - Mr Aakash Gupta on " +
+                "the Humor Night of Effervescence'18!"
+
+        val desc_local = "Now it’s time to relive and cherish some memories yet again, for Effervescence recreates it back, as we " +
+                "present to you the night which is going to mark the enigma of ultimate fiesta, the one and only, The Local Train. " +
+                "Now as the house is up and the fire within rises, join in for some surreal experiences and invigoration of wonder for the " +
+                "grounds of Effervescence would be waiting for you!"
+
+        val pagerAdapter = GlazyFragmentPagerAdapter(childFragmentManager, context)
+
+        pagerAdapter.addCardItem(
+            GlazyCard()
+                .withTitle("CELEB NIGHT")
+                .withSubTitle("7th October")
+                .withDescription(desc_celeb.toUpperCase())
+                .withImageRes(imgSunidhi)
+                .withImageCutType(ImageCutType.WAVE)
+                .withImageCutHeightDP(50)
+        )
+
+        pagerAdapter.addCardItem(
+            GlazyCard()
+                .withTitle("ROCK NIGHT")
+                .withSubTitle("6th October")
+                .withDescription(desc_local.toUpperCase())
+                .withImageRes(imgRock)
+                .withImageCutType(ImageCutType.ARC)
+                .withImageCutHeightDP(50)
+        )
+
+
+        pagerAdapter.addCardItem(
+            GlazyCard()
+                .withTitle("HUMOUR NIGHT")
+                .withSubTitle("6th October")
+                .withDescription(desc_humour.toUpperCase())
+                .withImageRes(imgHumour)
+                .withImageCutType(ImageCutType.LINE_POSITIVE)
+                .withImageCutHeightDP(50)
+        )
+
+
+        pagerAdapter.addCardItem(
+            GlazyCard()
+                .withTitle("EDM NIGHT")
+                .withSubTitle("5th October")
+                .withDescription(desc_edm.toUpperCase())
+                .withImageRes(imgEDM)
+                .withImageCutType(ImageCutType.WAVE)
+                .withImageCutHeightDP(50)
+        )
+
+        pager.adapter = pagerAdapter
+        pager.pageMargin = Utils.dpToPx(context, 25)
+        pager.setPageTransformer(false, GlazyPagerTransformer())
+    }
+
+
+}
