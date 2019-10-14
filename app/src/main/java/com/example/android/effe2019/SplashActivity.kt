47a7f8efdf2465f415763c3e69dca51691f972cb
a-macc <iit2018121@iiita.ac.in>
2019-10-14 21:48:40 +0530

    Chhote mote changes

--- app/src/main/java/com/example/android/effe2019/SplashActivity.kt
+++ app/src/main/java/com/example/android/effe2019/SplashActivity.kt
@@ -1,9 +1,15 @@
 package com.example.android.effe2019
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_splash.*
+import org.jetbrains.anko.startActivity
 import java.io.Serializable
 
 
@@ -17,6 +23,7 @@ class SplashActivity : AppCompatActivity(), Serializable {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        startLogoAnimation()
         firebaseDatabaseReference = FirebaseDatabase.getInstance().reference
         updates = ArrayList()
         team = ArrayList()
@@ -24,11 +31,34 @@ class SplashActivity : AppCompatActivity(), Serializable {
         fetchData()
     }
 
+    private fun startLogoAnimation() {
+        val yTranslationDown = ObjectAnimator.ofFloat(effeLogoIV, "translationY", 150f)
+        val yTranslationUp = ObjectAnimator.ofFloat(effeLogoIV, "translationY", -130f)
+        val yTranslationDownToStart = ObjectAnimator.ofFloat(effeLogoIV, "translationY", 0f)
+
+        yTranslationDown.duration = 1500
+        yTranslationUp.duration = 3000
+        yTranslationUp.startDelay = 1500
+        yTranslationDownToStart.duration = 1500
+        yTranslationDownToStart.startDelay = 4500
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(yTranslationDown, yTranslationUp, yTranslationDownToStart)
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+//                super.onAnimationEnd(animation)
+                animatorSet.start()
+            }
+        })
+        animatorSet.start()
+    }
+
+
     private fun fetchData() {
         firebaseDatabaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 createArrayLists(dataSnapshot)
-                startLaunchActivity()
+                showFinishedAnimation()
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -75,4 +105,20 @@ class SplashActivity : AppCompatActivity(), Serializable {
             }
         }
     }
+
+    private fun showFinishedAnimation() {
+        spin_kit.visibility = View.GONE
+        animationView.setAnimation("checked_done.json")
+        animationView.loop(false)
+        animationView.playAnimation()
+        animationView.addAnimatorListener(AnimatorListenerAdapter(
+            onStart = { },
+            onEnd = {
+                startLaunchActivity()
+                finish()
+            },
+            onCancel = { },
+            onRepeat = { }))
+    }
+
 }
